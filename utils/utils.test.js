49381edd651ca2c971003e8b22cab0fb33ed79ee
Peter <ernst.pierre@joinhoney.com>
2018-07-24 12:50:06 +0800

    getting familiar with "expect" library

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,17 +1,24 @@
+const expect = require('expect');
 const utils = require('./utils');
 
 it('should add two numbers' , () => {
   let res = utils.add(33,11);
+  
+  expect(res).toBe(44).toBeA('number');  
 
-  if(res !== 44){
-    throw new Error(`Expected 44, but got ${res}.`);
-  }
 });
 
 it('should square a number' , () => {
   let res = utils.square(5);
 
-  if(res !== 25){
-    throw new Error(`Expected 25, but got ${res}.`);
-  }
+  expect(res).toBe(25).toBeA('number');
+});
+
+it('should verify first and last name are set' , () => {
+  let user = utils.setName({} , 'Ernst Pierre');
+
+  expect(user).toInclude({
+    firstName : 'Ernst',
+    lastName: 'Pierre',
+  });
 });
