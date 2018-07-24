d07e03ab342e15a3a1372efa340583d1a6c3f958
Peter <ernst.pierre@joinhoney.com>
2018-07-24 11:50:16 +0800

    first test case

---
+++ utils/utils.js
@@ -0,0 +1,3 @@
+module.exports.add = (a , b) =>  a + b;
+
+module.exports.square = (number) => number * number;
---
+++ utils/utils.test.js
@@ -0,0 +1,17 @@
+const utils = require('./utils');
+
+it('should add two numbers' , () => {
+  let res = utils.add(33,11);
+
+  if(res !== 44){
+    throw new Error(`Expected 44, but got ${res}.`);
+  }
+});
+
+it('should square a number' , () => {
+  let res = utils.square(5);
+
+  if(res !== 25){
+    throw new Error(`Expected 25, but got ${res}.`);
+  }
+});
